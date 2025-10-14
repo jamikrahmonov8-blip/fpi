63862924b651419b98c43c1eb9a40672c1b0fe79
Muhammadjamshed <jamikr31@gamil.com>
2025-10-14 15:15:11 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// function fact(a) {
+//     if(a==0){
+//         return 1 
+//     }
+//     return a * fact(a - 1)
+// }
+// console.log(fact(5));
+
+
+
+
+
+//  function power(a,b) {
+//             if(b == 0)
+//             {
+//                 return 1
+//             }
+//             return a * power(a,b-1) 
+//         }
+//         console.log(power(2,3));
+
+
+
+// function sumto(a) {
+//      if(a == 0 ){
+//         return 0
+
+//      }    
+//      return a + sumto(a - 1)
+// }
+// console.log(sumto(7));
+
+
+// function couter() {
+//     let cnt = 0
+//     return () => {
+//         cnt++
+//         return cnt
+//     }
+// }
+// let cont = couter()
+// console.log(cont());
+// console.log(cont());
+// console.log(cont());
+
+
+
+// function creat(a) {
+//     return (b) =>
+//     {
+//         let sum = a+b
+//         return sum
+//     }
+// }
+// console.log(creat(5)(3));
+
